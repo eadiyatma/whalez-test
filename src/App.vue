fd71454b6d82bf9b39493d97a06f532686c694a5
efran <efran@bitcoin.co.id>
2022-10-18 13:20:25 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">hello world</h1>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
